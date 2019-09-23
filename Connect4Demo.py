fcb74eea61cd27144558eced7c728b7f3ad64dfd
Kristoffer Magelssen <krislm15@uia.no>
2019-09-23 13:51:09 +0200

    Replaced tsetlin machine fitting code.
    The old version used the fitting code from the XOR example.
    I replaced it with the code from the MNIS example.
    It now also prints out the accuracy of each epoch,
    instead of only printing out the average accuracy at the end.

--- Connect4Demo.py
+++ Connect4Demo.py
@@ -1,5 +1,6 @@
 from pyTsetlinMachine.tm import MultiClassTsetlinMachine
 import numpy as np
+from time import time
 
 # Parameters
 split_ratio = 0.9
@@ -14,13 +15,16 @@ X_test = X[int(len(X) * split_ratio):]
 Y_train = Y[:int(len(Y) * split_ratio)]
 Y_test = Y[int(len(Y) * split_ratio):]
 
-tm = MultiClassTsetlinMachine(1000, 15, 3.9, boost_true_positive_feedback=0)
+tm = MultiClassTsetlinMachine(10000, 80, 27, boost_true_positive_feedback=0)
 
-tm.fit(X_train, Y_train, epochs=200)
+#tm.fit(X_train, Y_train, epochs=200)
+
+print("\nAccuracy over 200 epochs:\n")
+for i in range(200):
+	start = time()
+	tm.fit(X_train, Y_train, epochs=1, incremental=True)
+	stop = time()
+	result = 100*(tm.predict(X_test) == Y_test).mean()
+	print("#%d Accuracy: %.2f%% (%.2fs)" % (i+1, result, stop-start))
 
 print("Accuracy:", 100*(tm.predict(X_test) == Y_test).mean())
-
-print("Prediction: x1 = 1, x2 = 0, ... -> y = %d" % (tm.predict(np.array([[1,0,1,0,1,0,1,1,1,1,0,0]]))))
-print("Prediction: x1 = 0, x2 = 1, ... -> y = %d" % (tm.predict(np.array([[0,1,1,0,1,0,1,1,1,1,0,0]]))))
-print("Prediction: x1 = 0, x2 = 0, ... -> y = %d" % (tm.predict(np.array([[0,0,1,0,1,0,1,1,1,1,0,0]]))))
-print("Prediction: x1 = 1, x2 = 1, ... -> y = %d" % (tm.predict(np.array([[1,1,1,0,1,0,1,1,1,1,0,0]]))))
