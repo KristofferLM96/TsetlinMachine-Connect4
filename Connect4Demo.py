15800b00cb6850b570f51ca048076bda664be4a4
Kristoffer Magelssen <krislm15@uia.no>
2019-10-23 13:33:12 +0200

    Added latest version. It now prints more information.

--- Connect4Demo.py
+++ Connect4Demo.py
@@ -4,33 +4,30 @@ from time import time
 
 # Parameters
 split_ratio = 0.9
-#train_data = np.loadtxt("Data/bitcode-connect-4.data", delimiter=",")
+epochs = 100
+clauses = 10000  # 11502
+T = 80  # 30
+s = 27  # 58.98
 
-#X = train_data[:, 0:-1]
-#Y = train_data[:, -1]
+print("epochs = ", epochs)
+print("clauses = ", clauses)
+print("T = ", T)
+print("s = ", s, "\n")
 
-#X_train = X[:int(len(X) * split_ratio)]
-#X_test = X[int(len(X) * split_ratio):]
-
-#Y_train = Y[:int(len(Y) * split_ratio)]
-#Y_test = Y[int(len(Y) * split_ratio):]
-
-"""
-Audun sitt dataset.
-"""
-# *********************************************************************************** #
-train = np.loadtxt("Data/trainingdata.txt", delimiter=",")
-test = np.loadtxt("Data/testdata.txt", delimiter=",")
+print("Loading training data..")
+train = np.loadtxt("Data/eventrain.data", delimiter=",")
+print("Loading test data..")
+test = np.loadtxt("Data/eventest.data", delimiter=",")
 X_train = train[:, 0:-1]
 X_test = test[:, 0:-1]
 Y_train = train[:, -1]
 Y_test = test[:, -1]
-# *********************************************************************************** #
 
-tm = MultiClassTsetlinMachine(10000, 80, 27, boost_true_positive_feedback=0)
-
-print("\nAccuracy over 50 epochs:\n")
-for i in range(50):
+print("Creating MultiClass Tsetlin Machine.")
+tm = MultiClassTsetlinMachine(clauses, T, s, boost_true_positive_feedback=0)
+print("Starting TM..")
+print("\nAccuracy over ", epochs, " epochs:\n")
+for i in range(epochs):
 	start = time()
 	tm.fit(X_train, Y_train, epochs=1, incremental=True)
 	stop = time()
@@ -38,3 +35,5 @@ for i in range(50):
 	print("#%d Accuracy: %.2f%% (%.2fs)" % (i+1, result, stop-start))
 
 print("Mean Accuracy:", 100*(tm.predict(X_test) == Y_test).mean())
+print("Finished running..")
+print("Shutting down.")
