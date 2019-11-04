f77c8031428d700a01191699a32502f85ec97342
Kristoffer Magelssen <krislm15@uia.no>
2019-11-04 11:42:39 +0100

    Moved the code for TM into a functin. Makes it easier when running k-fold.

--- Connect4Demo.py
+++ Connect4Demo.py
@@ -23,17 +23,22 @@ X_test = test[:, 0:-1]
 Y_train = train[:, -1]
 Y_test = test[:, -1]
 
-print("Creating MultiClass Tsetlin Machine.")
-tm = MultiClassTsetlinMachine(clauses, T, s, boost_true_positive_feedback=0)
-print("Starting TM..")
-print("\nAccuracy over ", epochs, " epochs:\n")
-for i in range(epochs):
-	start = time()
-	tm.fit(X_train, Y_train, epochs=1, incremental=True)
-	stop = time()
-	result = 100*(tm.predict(X_test) == Y_test).mean()
-	print("#%d Accuracy: %.2f%% (%.2fs)" % (i+1, result, stop-start))
 
-print("Mean Accuracy:", 100*(tm.predict(X_test) == Y_test).mean())
-print("Finished running..")
-print("Shutting down.")
+def TM(_clauses, _T, _s, _epochs):
+	print("Creating MultiClass Tsetlin Machine.")
+	tm = MultiClassTsetlinMachine(_clauses, _T, _s, boost_true_positive_feedback=0)
+	print("Starting TM..")
+	print("\nAccuracy over ", _epochs, " epochs:\n")
+	for i in range(_epochs):
+		start = time()
+		tm.fit(X_train, Y_train, epochs=1, incremental=True)
+		stop = time()
+		result = 100*(tm.predict(X_test) == Y_test).mean()
+		print("#%d Accuracy: %.2f%% (%.2fs)" % (i+1, result, stop-start))
+
+	print("Mean Accuracy:", 100*(tm.predict(X_test) == Y_test).mean())
+	print("Finished running..")
+	print("Shutting down.")
+
+
+ConvTM(clauses, T, s, epochs)
