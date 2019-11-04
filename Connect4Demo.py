700fd9786a02ec16d9593888657f5e0179849ab5
Kristoffer Magelssen <krislm15@uia.no>
2019-11-04 12:32:42 +0100

    Moved loading of dataset into its own function.

--- Connect4Demo.py
+++ Connect4Demo.py
@@ -14,31 +14,49 @@ print("clauses = ", clauses)
 print("T = ", T)
 print("s = ", s, "\n")
 
-print("Loading training data..")
-train = np.loadtxt("Data/eventrain.data", delimiter=",")
-print("Loading test data..")
-test = np.loadtxt("Data/eventest.data", delimiter=",")
-X_train = train[:, 0:-1]
-X_test = test[:, 0:-1]
-Y_train = train[:, -1]
-Y_test = test[:, -1]
+X_train = np.array([])
+Y_train = np.array([])
+X_test = np.array([])
+Y_test = np.array([])
+
+path_train = "Data/eventrain.data"
+path_test = "Data/eventest.data"
+
+
+def loading_data(_path_train, _path_test):
+    print("Loading training data..")
+    train_data = np.loadtxt(_path_train, delimiter=",")
+    print("..using train dataset: ", _path_train)
+    global X_train
+    global Y_train
+    X_train = train_data[:, 0:-1]
+    Y_train = train_data[:, -1]
+
+    print("Loading test data..")
+    test_data = np.loadtxt(_path_test, delimiter=",")
+    print("..using test dataset: ", _path_test)
+    global X_test
+    global Y_test
+    X_test = test_data[:, 0:-1]
+    Y_test = test_data[:, -1]
 
 
 def TM(_clauses, _T, _s, _epochs):
-	print("Creating MultiClass Tsetlin Machine.")
-	tm = MultiClassTsetlinMachine(_clauses, _T, _s, boost_true_positive_feedback=0)
-	print("Starting TM..")
-	print("\nAccuracy over ", _epochs, " epochs:\n")
-	for i in range(_epochs):
-		start = time()
-		tm.fit(X_train, Y_train, epochs=1, incremental=True)
-		stop = time()
-		result = 100*(tm.predict(X_test) == Y_test).mean()
-		print("#%d Accuracy: %.2f%% (%.2fs)" % (i+1, result, stop-start))
+    print("Creating MultiClass Tsetlin Machine.")
+    tm = MultiClassTsetlinMachine(_clauses, _T, _s, boost_true_positive_feedback=0)
+    print("Starting TM..")
+    print("\nAccuracy over ", _epochs, " epochs:\n")
+    for i in range(_epochs):
+        start = time()
+        tm.fit(X_train, Y_train, epochs=1, incremental=True)
+        stop = time()
+        result = 100 * (tm.predict(X_test) == Y_test).mean()
+        print("#%d Accuracy: %.2f%% (%.2fs)" % (i + 1, result, stop - start))
 
-	print("Mean Accuracy:", 100*(tm.predict(X_test) == Y_test).mean())
-	print("Finished running..")
-	print("Shutting down.")
+    print("Mean Accuracy:", 100 * (tm.predict(X_test) == Y_test).mean())
+    print("Finished running..")
+    print("Shutting down.")
 
 
-ConvTM(clauses, T, s, epochs)
+loading_data(path_train, path_test)
+TM(clauses, T, s, epochs)
