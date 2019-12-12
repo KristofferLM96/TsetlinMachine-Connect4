64d5bd94abf0bb80a7b629a5747ce798748dd4a4
Kristoffer Magelssen <krislm15@uia.no>
2019-12-12 12:04:35 +0100

    Added new TM with k fold support.

--- TM.py
+++ TM.py
@@ -8,6 +8,7 @@ epochs = 100
 clauses = 10000  # 11502
 T = 80  # 30
 s = 27  # 58.98
+k_fold_amount = 10
 
 print("epochs = ", epochs)
 print("clauses = ", clauses)
@@ -19,26 +20,40 @@ Y_train = np.array([])
 X_test = np.array([])
 Y_test = np.array([])
 
-path_train = "Data/eventrain.data"
-path_test = "Data/eventest.data"
+base_path = "statickfold.data"
+# path_train = "Data/eventrain.data"
+# path_test = "Data/eventest.data"
 
 
-def loading_data(_path_train, _path_test):
+def merging_k_fold(file_amount, _clauses, _T, _s, _epochs):
+    results = []
+    for i in range(file_amount):
+        train_string = "Data/" + str(i) + "train" + base_path
+        test_string = "Data/" + str(i) + "test" + base_path
+        score = loading_data(train_string, test_string, _clauses, _T, _s, _epochs)
+        results.append(score)
+
+    return results
+
+
+def loading_data(_train, _test, _clauses, _T, _s, _epochs):
     print("Loading training data..")
-    train_data = np.loadtxt(_path_train, delimiter=",")
-    print("..using train dataset: ", _path_train)
+    train_data = np.loadtxt(_train, delimiter=",")
+    # print("..using train dataset: ", _path_train)
     global X_train
     global Y_train
     X_train = train_data[:, 0:-1]
     Y_train = train_data[:, -1]
 
     print("Loading test data..")
-    test_data = np.loadtxt(_path_test, delimiter=",")
-    print("..using test dataset: ", _path_test)
+    test_data = np.loadtxt(_test, delimiter=",")
+    # print("..using test dataset: ", _path_test)
     global X_test
     global Y_test
     X_test = test_data[:, 0:-1]
     Y_test = test_data[:, -1]
+
+    return TM(_clauses, _T, _s, epochs)
 
 
 def TM(_clauses, _T, _s, _epochs):
@@ -58,5 +73,5 @@ def TM(_clauses, _T, _s, _epochs):
     print("Shutting down.")
 
 
-loading_data(path_train, path_test)
-TM(clauses, T, s, epochs)
+score = merging_k_fold(k_fold_amount, clauses, T, s, epochs)
+print(score)
