4dd1b31871c6e5cb37c8cbf99dbbf7e4714a0f67
Kristoffer Magelssen <krislm15@uia.no>
2019-12-16 19:47:27 +0100

    Added conv part of clauses print app. Still not runnable.

--- TM_conv_clauses.py
+++ TM_conv_clauses.py
@@ -1,9 +1,16 @@
+shape_y = 6
+shape_x = 7
+shape_z = 2
+window_x = 4
+window_y = 4
+
+
 def Rearrange(WrongList):
     output = []
-    for column in range(6):
-        temp = 6 - column
-        for row in range(7):
-            index = (6 * row) + temp
+    for column in range(shape_y):
+        temp = shape_y - column
+        for row in range(shape_x):
+            index = (shape_y * row) + temp
             # print(index)
             output.append(WrongList[index - 1])
     return output
@@ -13,10 +20,10 @@ def transform(input):
     # print(input)
     if (int(input[0]) and int(input[2])) or (int(input[1]) and int(input[3])):
         return "Fa"
-    elif (int(input[0]) and int(input[1])):
-        return "+#"
-    elif (int(input[2]) and int(input[3])):
-        return "-#"
+    elif int(input[0]) and int(input[1]):
+        return "#"
+    elif int(input[2]) and int(input[3]):
+        return "b"
     elif int(input[0]):
         return "+X"
     elif int(input[1]):
@@ -26,24 +33,43 @@ def transform(input):
     elif int(input[3]):
         return "-o"
     else:
-        return "*#"
+        return "*"
 
 
 def GetOutput(tm, tm_class, clause):
     output = []
-    for i in range(84 * 2):
-        outputbit = tm.ta_action(tm_class, clause, i)
-        output.append(outputbit)
+    xyz_id_old = 0
+    for i in range(5):
+        print(tm.ta_action(tm_class, clause, i))
+    for y in range(window_y):
+        for x in range(window_x):
+            for z in range(shape_z):
+                xyz_id = (shape_x - window_x) + (shape_y - window_y) + (y * window_x * shape_z) + (x * shape_z) + z
+                output_bit = tm.ta_action(tm_class, clause, xyz_id)
+                print("Non-Negated")
+                print(xyz_id)
+                output.append(output_bit)
+                xyz_id_old = xyz_id + 1
+    output_pos_neg = GetOutput_negated(tm, tm_class, clause, xyz_id_old, output)
+    return output_pos_neg
+
+
+def GetOutput_negated(tm, tm_class, clause, xyz_id_old, output):
+    for y in range(window_y):
+        for x in range(window_x):
+            for z in range(shape_z):
+                xyz_id = xyz_id_old + (shape_x - window_x) + (shape_y - window_y) + (y * window_x * shape_z) \
+                         + (x * shape_z) + z
+                output_bit = tm.ta_action(tm_class, clause, xyz_id)
+                print("Negated")
+                print(xyz_id)
+                output.append(output_bit)
     return output
-
-
-def PrintClause(clause):
-    for i in clause:
-        print(i)
 
 
 def Align(tm, tm_class, clause):
     output = GetOutput(tm, tm_class, clause)
+    print(output)
     nonNegated = output[:int(len(output) / 2)]
     negated = output[int(len(output) / 2):]
     xbit = Rearrange(nonNegated[:int(len(nonNegated) / 2)])
@@ -51,12 +77,18 @@ def Align(tm, tm_class, clause):
     nxbit = Rearrange(negated[:int(len(negated) / 2)])
     nobit = Rearrange(negated[int(len(negated) / 2):])
     board = []
-    for i in range(42):
-        resultclauses.write(str(xbit[i]) + str(obit[i]) + str(nxbit[i]) + str(nobit[i]))
-        if i < 41:
+    for i in range(window_x * window_y):
+        print(i)
+        # resultclauses.write(str(xbit[i]) + str(obit[i]) + str(nxbit[i]) + str(nobit[i]))
+        if i < (window_x * window_y) - 1:
             resultclauses.write(",")
         else:
             resultclauses.write("\n")
+
+
+def PrintClause(clause):
+    for i in clause:
+        print(i)
 
 
 def PrintClass(Ts, Class, clauses):
@@ -82,3 +114,4 @@ with open("3Dresults/2D" + timestr + "clauses.csv") as f:
             temp = temp + transform(table[1][i * 7 + j]) + " "
             # print(i*7+j)
         print(temp)
+
