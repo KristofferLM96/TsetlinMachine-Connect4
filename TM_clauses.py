47094140714f60ad16bad80b56324c2bf34803a3
Kristoffer Magelssen <krislm15@uia.no>
2019-12-19 20:13:37 +0100

    It can now read from clauses in csv file and transform it into readable clauses put inside the game board. Prints out the results.

--- TM_clauses.py
+++ TM_clauses.py
@@ -1,9 +1,15 @@
+import csv
+
+shape_y = 6
+shape_x = 7
+
+
 def Rearrange(WrongList):
     output = []
-    for column in range(6):
-        temp = 6 - column
-        for row in range(7):
-            index = (6 * row) + temp
+    for column in range(shape_y):
+        temp = shape_y - column
+        for row in range(shape_x):
+            index = (shape_y * row) + temp
             # print(index)
             output.append(WrongList[index - 1])
     return output
@@ -13,9 +19,9 @@ def transform(input):
     # print(input)
     if (int(input[0]) and int(input[2])) or (int(input[1]) and int(input[3])):
         return "Fa"
-    elif (int(input[0]) and int(input[1])):
+    elif int(input[0]) and int(input[1]):
         return "+#"
-    elif (int(input[2]) and int(input[3])):
+    elif int(input[2]) and int(input[3]):
         return "-#"
     elif int(input[0]):
         return "+X"
@@ -46,17 +52,17 @@ def Align(tm, tm_class, clause):
     output = GetOutput(tm, tm_class, clause)
     nonNegated = output[:int(len(output) / 2)]
     negated = output[int(len(output) / 2):]
-    xbit = Rearrange(nonNegated[:int(len(nonNegated) / 2)])
-    obit = Rearrange(nonNegated[int(len(nonNegated) / 2):])
-    nxbit = Rearrange(negated[:int(len(negated) / 2)])
-    nobit = Rearrange(negated[int(len(negated) / 2):])
+    xbit = nonNegated[:int(len(nonNegated) / 2)]
+    obit = nonNegated[int(len(nonNegated) / 2):]
+    nxbit = negated[:int(len(negated) / 2)]
+    nobit = negated[int(len(negated) / 2):]
     board = []
     for i in range(42):
-        resultclauses.write(str(xbit[i]) + str(obit[i]) + str(nxbit[i]) + str(nobit[i]))
+        print(str(xbit[i]) + str(obit[i]) + str(nxbit[i]) + str(nobit[i]))
         if i < 41:
-            resultclauses.write(",")
+            print(",")
         else:
-            resultclauses.write("\n")
+            print("\n")
 
 
 def PrintClass(Ts, Class, clauses):
@@ -67,18 +73,21 @@ def PrintClass(Ts, Class, clauses):
         # PrintClause(action)
 
 
-PrintClass(tm, 1, clauses)
-resultclauses.close()
+# PrintClass(tm, 1, clauses)
 
-with open("3Dresults/2D" + timestr + "clauses.csv") as f:
+with open("Data/2D1218-0437clauses1.csv") as f:
     reader = csv.reader(f)
     table = []
     for row in reader:
         table.append(row)
-    print(table[1])
-    for i in range(6):
-        temp = ""
-        for j in range(7):
-            temp = temp + transform(table[1][i * 7 + j]) + " "
-            # print(i*7+j)
-        print(temp)
+    index = 1
+    for index in range(100):
+        print(table[index])
+        for i in range(6):
+            temp = ""
+            for j in range(7):
+                temp = temp + transform(table[index][i * 7 + j]) + " "
+                # print(i*7+j)
+            print(temp)
+        print("\n")
+        index = index + 1
